141332f268d717126cbf8a0c63f4a9c9888890ee
JC Snider <jcsnider@users.noreply.github.com>
2018-11-21 22:29:12 -0500

    Made nud value property update whenever text was changed instead of on invalidation
    Fixed styling error of nud arrows when they were moved around due to scrolling

--- DarkUI/Controls/DarkNumericUpDown.cs
+++ DarkUI/Controls/DarkNumericUpDown.cs
@@ -16,6 +16,14 @@ namespace DarkUI.Controls
         private bool mouseDown = false;
         private Point mousePos = new Point();
         private Rectangle scrollButtons;
+        private bool settingText = false;
+
+        public virtual Decimal Value
+        {
+            set { base.Value = Math.Min(Maximum, Math.Max(Minimum, value)); }
+            get { return base.Value; }
+        }
+
         public DarkNumericUpDown()
         {
             this.ForeColor = Color.Gainsboro;
@@ -27,6 +35,8 @@ namespace DarkUI.Controls
             this.MouseMove += DarkNumericUpDown_MouseMove;
             this.MouseUp += DarkNumericUpDown_MouseUp;
             this.MouseDown += DarkNumericUpDown_MouseDown;
+            this.TextChanged += DarkNumericUpDown_TextChanged;
+            
             try
             {
                 // Prevent flickering, only if our assembly 
@@ -37,13 +47,36 @@ namespace DarkUI.Controls
 
                 if (method != null)
                 {
-                    object[] param = { ControlStyles.AllPaintingInWmPaint | ControlStyles.DoubleBuffer, true };
+                    object[] param = { ControlStyles.AllPaintingInWmPaint | ControlStyles.DoubleBuffer, false };
                     method.Invoke(this.Controls[0], param);
                 }
             }
             catch (SecurityException)
             {
                 // Don't do anything, we are running in a trusted contex.
+            }
+        }
+
+        private void DarkNumericUpDown_TextChanged(object sender, EventArgs e)
+        {
+            if (!settingText)
+            {
+                settingText = true;
+                Decimal d = 0;
+                if (decimal.TryParse(this.Text, out d))
+                {
+                    var txtBox = this.Controls.OfType<TextBox>().FirstOrDefault() as TextBox;
+                    var carotPos = txtBox.SelectionStart;
+                    Value = d;
+                    if (Value == this.Maximum)
+                    {
+                        this.Text = Value.ToString();
+                    }
+
+                    this.Select(carotPos, 0);
+                }
+
+                settingText = false;
             }
         }
 
@@ -64,10 +97,11 @@ namespace DarkUI.Controls
 
         private void DarkNumericUpDown_Paint(object sender, PaintEventArgs e)
         {
-            e.Graphics.FillRectangle(new SolidBrush(Colors.DarkBackground), new Rectangle(0, 0, e.ClipRectangle.Width,e.ClipRectangle.Height));
+            var rect = new Rectangle(0, 0, this.Controls[0].Width, this.Controls[0].Height);
+            e.Graphics.FillRectangle(new SolidBrush(Colors.DarkBackground), new Rectangle(0, 0, rect.Width,rect.Height));
 
             var upIcon = ScrollIcons.scrollbar_arrow_standard;
-            var upRect = new Rectangle(e.ClipRectangle.Width/2 - upIcon.Width/2,e.ClipRectangle.Height/4 - upIcon.Height/2,upIcon.Width,upIcon.Height);
+            var upRect = new Rectangle(rect.Width/2 - upIcon.Width/2,rect.Height/4 - upIcon.Height/2,upIcon.Width,upIcon.Height);
             upIcon = upRect.Contains(mousePos) ? ScrollIcons.scrollbar_arrow_hot : ScrollIcons.scrollbar_arrow_standard;
 
             if (mouseDown && upRect.Contains(mousePos))
@@ -80,14 +114,14 @@ namespace DarkUI.Controls
 
             // Down arrow
             var downIcon = ScrollIcons.scrollbar_arrow_standard;
-            var downRect = new Rectangle(e.ClipRectangle.Width / 2 - upIcon.Width / 2, e.ClipRectangle.Height / 2 + e.ClipRectangle.Height/4 - upIcon.Height / 2, upIcon.Width, upIcon.Height);
+            var downRect = new Rectangle(rect.Width / 2 - upIcon.Width / 2, rect.Height / 2 + rect.Height/4 - upIcon.Height / 2, upIcon.Width, upIcon.Height);
             downIcon = downRect.Contains(mousePos) ? ScrollIcons.scrollbar_arrow_hot : ScrollIcons.scrollbar_arrow_standard;
 
             if (mouseDown && downRect.Contains(mousePos))
                 downIcon = ScrollIcons.scrollbar_arrow_clicked;
 
             e.Graphics.DrawImageUnscaled(downIcon, downRect);
-            scrollButtons = e.ClipRectangle;
+            scrollButtons = rect;
         }
 
         protected override void OnPaint(PaintEventArgs e)
